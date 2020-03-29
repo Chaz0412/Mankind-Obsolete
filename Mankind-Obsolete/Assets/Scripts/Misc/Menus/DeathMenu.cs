089a6a0d47a8febcfcbd469618f7c389e3ea6f78
Dommax17 <dommax17@users.noreply.github.com>
2020-03-29 18:09:02 +0100

    Death/Pause Menu Implementation to Level 1 and 3

---
+++ Mankind-Obsolete/Assets/Scripts/Misc/Menus/DeathMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathMenu : MonoBehaviour
+{
+    
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        //insert code to reset level
+        gameObject.SetActive(false);
+        AudioListener.pause = false;
+    }
+
+    public void QuitToMain()
+    {
+        Time.timeScale = 1f;
+        AudioListener.pause = false;
+        //insert code to quit to main
+    }
+}
--- Mankind-Obsolete/Assets/Scripts/Player/Player.cs
+++ Mankind-Obsolete/Assets/Scripts/Player/Player.cs
@@ -8,7 +8,9 @@ public class Player : MonoBehaviour
     public int currentHealth;
 
     public HealthBar healthBar;
-    public GameObject deathEffect;
+    //public GameObject deathEffect;
+    public DeathMenu deathMenu;
+    public PauseMenu pauseMenu;
 
     void Start()
     {
@@ -19,6 +21,15 @@ public class Player : MonoBehaviour
     void Update()
     {
         healthBar.SetHealth(currentHealth);
+
+        if(Input.GetKeyDown(KeyCode.Escape) && pauseMenu.gameObject.activeInHierarchy == false)
+        {
+            pauseMenu.Pause();
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && pauseMenu.gameObject.activeInHierarchy == true)
+        {
+            pauseMenu.Resume();
+        }
     }
 
     public void takeDamage (int damage)
@@ -29,6 +40,7 @@ public class Player : MonoBehaviour
         if (currentHealth <= 0)
         {
             Die();
+            deathMenu.gameObject.SetActive(true);
         }
     }
 
@@ -47,8 +59,10 @@ public class Player : MonoBehaviour
 
     void Die()
     {
-        Instantiate(deathEffect, transform.position, Quaternion.identity);
-        Destroy(gameObject);
-    }
+        //Instantiate(deathEffect, transform.position, Quaternion.identity);
+        //Destroy(gameObject);
+        Time.timeScale = 0f;
+        gameObject.SetActive(false);
 
+    }
 }
