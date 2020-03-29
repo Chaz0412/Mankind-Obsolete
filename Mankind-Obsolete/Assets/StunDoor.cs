dd653f578e4a4ff1dff99b5aa573c906df8506e4
ShaunCuthbert <55137476+ShaunCuthbert@users.noreply.github.com>
2020-03-29 18:41:07 +0100

    Door?

---
+++ Mankind-Obsolete/Assets/StunDoor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StunDoor : MonoBehaviour
+{
+    Animator animator;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Stun")
+        {
+            print("Door hit");
+            animator.SetTrigger("Open");
+            gameObject.SetActive(false);
+        }
+    }
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.tag == "Stun")
+    //    {
+    //        print("Door hit");
+    //        animator.Play("DoorOpen");
+    //        gameObject.SetActive(false);
+    //    }
+    //}
+}
