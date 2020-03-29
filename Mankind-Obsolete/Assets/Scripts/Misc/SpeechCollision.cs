1626216d97d912e8a34332d39f0ac880ba80b7aa
Dommax17 <dommax17@users.noreply.github.com>
2020-03-29 19:43:58 +0100

    Level 1 text update and collider

---
+++ Mankind-Obsolete/Assets/Scripts/Misc/SpeechCollision.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechCollision : MonoBehaviour
+{
+    public Player player;
+    public GameObject thing;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            thing.SetActive(true);
+        }
+    }
+}
