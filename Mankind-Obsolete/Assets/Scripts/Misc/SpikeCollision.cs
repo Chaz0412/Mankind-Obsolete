22ceecd5a1b072bcac128cc62873de86bdb342c1
Dommax17 <dommax17@users.noreply.github.com>
2020-03-29 18:41:38 +0100

    Spike Collisions

---
+++ Mankind-Obsolete/Assets/Scripts/Misc/SpikeCollision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeCollision : MonoBehaviour
+{
+    public Player player;
+    public float x;
+    public float y;
+    public float z;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            player.takeDamage(10);
+            player.transform.position = new Vector3(x, y, z);
+        }
+    }
+}
